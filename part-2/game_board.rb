c4ab0fb96421d7cb58be7bb363fb5a1a07776ab3
Mona AlMukhaizeem <email": null>
2016-12-20 16:34:31 -0500

    draft one of part 2

--- part-2/game_board.rb
+++ part-2/game_board.rb
@@ -1,6 +1,7 @@
 require_relative 'terrain_helper'
-
+require 'pry'
 class GameBoard
+
   attr_reader :terrain_helper
   attr_reader :terrain_list
   attr_reader :size
@@ -9,5 +10,22 @@ class GameBoard
     @terrain_list = args[:terrain_list]
     @terrain_helper = args.fetch(:terrain_helper, TerrainHelper.new)
     @size = args[:size]
+  end
+
+  def terrain_map
+   terrain_list.map{|letter| terrain_helper.letter_to_terrain(letter)}.each_slice(size).to_a
+  end
+# binding.pry
+  def difficulty_of_journey(journey)
+  end
+
+  private
+
+  def Journey_to_letters(journey)
+    #takes the journey
+    #find each index location
+    #convert to letter
+
+
   end
 end
